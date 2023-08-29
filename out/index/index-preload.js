22ac87aae6971eb76da6eb6199fc4e59d8597cb5
kevinthmm <kevinthmm@gmail.com>
2023-08-29 16:50:34 +0800

    optimised the build process to compile tailwind css as well.

--- out/index/index-preload.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld("electronAPI", {
-    grabTimetable: (email, password) => electron_1.ipcRenderer.invoke("grab-timetable", email, password),
-});
-//# sourceMappingURL=index-preload.js.map
