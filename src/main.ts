89a8b7ae67fa3ba4c0cfa68e7605714991f8eb74
kevinthmm <kevinthmm@gmail.com>
2023-08-29 14:34:30 +0800

    minor fixes and testing new headless in puppeterr

--- src/main.ts
+++ src/main.ts
@@ -44,7 +44,7 @@ async function grabTimetable(email: string, password: string) {
 
   const browser = await puppeteer.launch({
     executablePath: revisionInfo.executablePath,
-    headless: true,
+    headless: "new",
     args: ["--start-maximized", "--window-size=1920,1080"],
     defaultViewport: null,
   });
